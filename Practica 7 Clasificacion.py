2286d454792c651e41e85854c5b39f6bd95caa22
Ivrohm <72846342+Ivrohm@users.noreply.github.com>
2023-05-20 23:25:07 -0600

    Add files via upload

--- Practica 7 Clasificacion.py
+++ Practica 7 Clasificacion.py
@@ -6,8 +6,9 @@ import statsmodels.formula.api as line
 
 
 df = pd.read_csv('columnas_tratar.csv')
-
-x= df[[ 'NA_Sales','EU_Sales','JP_Sales','Other_Sales']]
-y = df['Global_Sales']
-
-d
+#df.rename(columns={"Name":"Titulo","Platform" :"Plataforma", "Year_of_Release":"Año_de_lanzamiento", "Genre":"Genero", "Publisher":"Pubicador", "NA_Sales":"NA_Ventas",
+ #"Global_Sales":"Ventas_Globales"}, inplace=True)
+plataforma = df[[ 'NA_Sales','EU_Sales','JP_Sales','Other_Sales','Year_of_Release']]
+print(plataforma)
+plt.scatter(plataforma['Year_of_Release'],plataforma['NA_Sales'])
+plt.show()
